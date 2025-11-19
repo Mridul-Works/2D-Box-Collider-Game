88cfd138a52a5fd0e3d545176eec2f22d8280018
Mridul-Works <mridul2431@gmail.com>
2025-11-20 01:46:13 +0530

    game moving player and enemy funtionality added

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ class Player extends Box {
         super(50, "blue");
         this.x = 0;
         this.y = 225;
+        this.speed = 0;
+    }
+
+    move() {
+        this.x += this.speed;
+        // keep player inside canvas
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.size > paintbox.width) this.x = paintbox.width - this.size;
     }
 }
 
@@ -42,30 +50,52 @@ class Enemy extends Box {
 }
 
 let player = new Player();
-let e1 = new Enemy(1);
-let e2 = new Enemy(2);
+let e1 = new Enemy(4);
+let e2 = new Enemy(6);
+let e3 = new Enemy(12);
 
 e1.x = 120;
 e2.x = 240;
+e3.x = 360;
+// ensure initial y values
 e1.y = 0;
 e2.y = 200;
+e3.y = 100;
 
 function drawBox(box) {
     context.fillStyle = box.color;
     context.fillRect(box.x, box.y, box.size, box.size);
 }
 
-function updateGame() {
-    requestAnimationFrame(updateGame);
+// use correct method name: addEventListener
+// on mousedown -> move right; on mouseup -> stop
+paintbox.addEventListener("pointerdown", () => {
+    player.speed = 5; // change speed as needed
+});
+paintbox.addEventListener("pointerup", () => {
+    player.speed = 0;
+});
+paintbox.addEventListener("pointerleave", () => {
+    player.speed = 0;
+});
 
-    context.clearRect(0, 0, paintbox.width, paintbox.height);
-
+function gameLoop() {
+    // update
     e1.move();
     e2.move();
+    e3.move();
+    player.move();
 
+    // render (use canvas width/height, not hard-coded)
+    context.clearRect(0, 0, paintbox.width, paintbox.height);
     drawBox(player);
     drawBox(e1);
     drawBox(e2);
+    drawBox(e3);
+
+    // next frame
+    window.requestAnimationFrame(gameLoop);
 }
 
-updateGame();
+// start loop
+gameLoop();
