0a0b347d0eb6baccf81ca77a91f1749d532a59ac
Mridul-Works <mridul2431@gmail.com>
2025-11-20 01:30:17 +0530

    bounce back added

--- script.js
+++ script.js
@@ -23,28 +23,49 @@ class Enemy extends Box {
         super(50, "red");
         this.speed = speed;
     }
+
+    move() {
+        this.y += this.speed;
+
+        // bounce bottom
+        if (this.y + this.size > paintbox.height) {
+            this.y = paintbox.height - this.size;
+            this.speed = -Math.abs(this.speed);
+        }
+
+        // bounce top
+        if (this.y < 0) {
+            this.y = 0;
+            this.speed = Math.abs(this.speed);
+        }
+    }
 }
 
 let player = new Player();
-let e1 = new Enemy(10);
-let e2 = new Enemy(20);
+let e1 = new Enemy(1);
+let e2 = new Enemy(2);
 
 e1.x = 120;
 e2.x = 240;
+e1.y = 0;
+e2.y = 200;
 
 function drawBox(box) {
     context.fillStyle = box.color;
     context.fillRect(box.x, box.y, box.size, box.size);
 }
 
-setInterval(() => {
-    context.clearRect(0, 0, 500, 500);
+function updateGame() {
+    requestAnimationFrame(updateGame);
 
-    e1.y += e1.speed;
-    e2.y += e2.speed;
+    context.clearRect(0, 0, paintbox.width, paintbox.height);
+
+    e1.move();
+    e2.move();
 
     drawBox(player);
     drawBox(e1);
     drawBox(e2);
-}, 100);
+}
 
+updateGame();
